b3e3cfd6f44247e0efb11b689e304add2d096623
seanl88 <31197594+seanl88@users.noreply.github.com>
2017-10-11 14:59:28 -0500

    Add files via upload

---
+++ Drill select_from_where.sql
@@ -0,0 +1,31 @@
+-- The ID's and durations for all trips of duration greater than 500, ordered by duration.
+
+Select 
+	trip_id,
+	duration
+From 
+	trips
+Where
+	duration > 500
+Order By 
+	duration DESC
+
+-- Every column of the stations table for station id 84.
+
+Select
+	*
+From
+	stations
+Where
+	station_id == 48
+
+
+-- The min temperatures of all the occurrences of rain in zip 94301.
+
+Select
+	MinTemperatureF
+From
+	weather
+Where
+	Events Like 'Rain' and zip == 94301
+
