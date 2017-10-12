f11095ba077b2c254911802b2f3f1b873a8443c5
seanl88 <31197594+seanl88@users.noreply.github.com>
2017-10-12 12:30:28 -0500

    Add files via upload

---
+++ Drill- Aggregates and Groups.sql
@@ -0,0 +1,57 @@
+-- What was the hottest day in our data set? Where was that?
+
+Select
+
+    weather.Date,
+    MAX(MaxTemperatureF)
+
+From
+    
+    weather
+
+-- How many trips started at each station?
+
+Select
+
+	start_station,
+	Count(trip_id)
+
+From
+
+	trips
+
+Group by
+
+	start_station
+
+
+-- What's the shortest trip that happened?
+
+Select
+
+	trip_id,
+	MIN(duration)
+
+From
+
+	trips
+
+
+-- What is the average trip duration, by end station?
+
+Select
+
+	end_station,
+	(AVG(duration))
+
+From
+
+	trips
+
+Group by
+
+	end_station
+
+
+
+
